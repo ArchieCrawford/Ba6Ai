0e1d59edbddf10f9f7b8911f2cdd9f9031c09253
Archie Crawford <Archie.Crawford1@gmail.com>
2026-05-01 21:17:27 -0400

    Convert Ba6ai from submodule to regular directory by removing nested .git

---
+++ Ba6ai/Ba6ai/Ba6aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ba6aiApp.swift
+//  Ba6ai
+//
+//  Created by Archie Crawford on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Ba6aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ba6ai/Ba6ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ba6ai
+//
+//  Created by Archie Crawford on 4/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
